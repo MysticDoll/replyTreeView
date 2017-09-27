a92e6de76a0ef69724761596fe9915c3db3f108b
Mitsuru Takigahira <mysticdoll@mysticdoll.com>
2017-09-27 17:44:08 +0900

    implement api scraping

---
+++ server/getTwitterGuestToken.js
@@ -0,0 +1,43 @@
+const chromeLauncher = require("chrome-launcher");
+const CDP = require("chrome-remote-interface");
+
+module.exports = function(statusId) {
+  let url = `https://mobile.twitter.com/placeholdername/status/${statusId}`;
+  return new Promise((resolve, reject) => {
+    chromeLauncher.launch({
+      startingUrl: "target:blank",
+      chromeFlags: ["--headless", "--disable-gpu", "--no-sandbox"]
+    }).then(chrome => {
+      const options = {port: chrome.port};
+      CDP(options, client => {
+        const {Network, Page} = client;
+        Network.requestIntercepted(e => {
+          if(e.request.headers.authorization && e.request.headers["x-guest-token"]){
+            chrome.kill();
+            resolve({
+              authorization: e.request.headers.authorization,
+              "x-guest-token": e.request.headers["x-guest-token"]
+            });
+          }
+          Network.continueInterceptedRequest({interceptionId: e.interceptionId});
+        });
+        Promise.all([
+          Network.enable(),
+          Page.enable(),
+          Network.setRequestInterceptionEnabled({
+            enabled: true,
+            patterns:["https://api.twitter.com/2/timeline/conversation/"]
+          })
+        ]).then(() => {
+          Page.navigate({url: url});
+          Page.loadEventFired(() => {
+            setTimeout(() => {
+              chrome.kill().then(() => reject(new Error("timeout"))); 
+            }, 180000);
+          });
+        });
+      })
+    })
+    
+  });
+};
